ef0603c82dd771a27d0dc7d8c14059e629a4b7b2
Sergio Vega <stbnlen@gmail.com>
2021-03-01 22:24:18 -0300

    doc rename

---
+++ docs/precache-manifest.7bbc1bbc4ead19a4c2242c1150f4b7c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cfafa1a2bba1639b4da8359f1e6010b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae36b6ea146eb870e018",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae36b6ea146eb870e018",
+    "url": "/static/js/main.07917db6.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
